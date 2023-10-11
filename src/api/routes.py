7f38775ce76fc45c75404784798bcf7e78b97a3d
Ernesto Medina <ernesto.m.medina.m@gmail.com>
2023-10-11 16:27:22 +0000

    :package: Installs flask-jwt-extended and updates code

--- src/api/routes.py
+++ src/api/routes.py
@@ -1,29 +1,14 @@
 from flask import request, jsonify, Blueprint
 from .models import db, User
 from .utils import generate_sitemap, APIException
-import jwt
 import datetime
 from functools import wraps
+# import jwt
+from flask_jwt_extended import create_access_token, jwt_required, current_user
 
 api = Blueprint('api', __name__)
 
-# Define a function to check for the presence of a JWT token in the request headers
-def token_required(f):
-    @wraps(f)
-    def decorated(*args, **kwargs):
-        token = request.headers.get('Authorization')
-        if not token:
-            return jsonify({'message': 'Token is missing'}), 401
-
-        try:
-            data = jwt.decode(token, app.config['SECRET_KEY'], algorithms=["HS256"])
-            current_user = User.query.filter_by(id=data['user_id']).first()
-        except:
-            return jsonify({'message': 'Token is invalid'}), 401
-
-        return f(current_user, *args, **kwargs)
-
-    return decorated
+# moved the custom jwt loader to app.py
 
 @api.route('/hello', methods=['POST', 'GET'])
 def handle_hello():
@@ -32,7 +17,10 @@ def handle_hello():
     }
     return jsonify(response_body), 200
 
-@api.route('/api/signup', methods=['POST'])
+# @api.route('/api/signup', methods=['POST'])
+# you don't need the extra /api because that is being added on the 
+# app.py file where it says app.register_blueprint(api, url_prefix="/api")
+@api.route('/signup', methods=['POST'])
 def signup():
     data = request.get_json()
     email = data.get('email')
@@ -43,13 +31,32 @@ def signup():
     # hashed_password = hash_password_function(password)
 
     # Create a new user in the database and store the hashed password
-    user = User(email=email, password=hashed_password, is_active=True)
+    
+    # @ernestomedinam:
+    # you are not hasing the user password :S
+    # to hash a password you need to create a random salt for 
+    # an user and then use their password + the salt and hash
+    # that hash is what you'd store in the database.
+    # in such case, checking password is valid consists on
+    # retrieving the user salt, adding it to the password the user
+    # sent on the log in request, hashing it using the 
+    # same method that you use when you create users,
+    # and then comparing if both hashes match (the one you just
+    # created and the one stored in database.)
+
+    # user = User(email=email, password=hashed_password, is_active=True)
+    # this is what it would look like using regular password and 
+    # not sending is_active=True, because your model __init__ function
+    # is not expecting it but rather just setting it to True always.
+    user = User(email=email, password=password)
     db.session.add(user)
     db.session.commit()
 
     return jsonify({'message': 'User created successfully'}), 201
 
-@api.route('/api/login', methods=['POST'])
+# @api.route('/api/login', methods=['POST'])
+# same as for sign up
+@api.route('/login', methods=['POST'])
 def login():
     data = request.get_json()
     email = data.get('email')
@@ -58,10 +65,26 @@ def login():
     # Check if the user exists and verify the password
     user = User.query.filter_by(email=email).first()
 
-    if user is None or not check_password(user.password, password):
+    # @ernestomedinam
+    # because you are not hashing, just compare both passwords
+    # if user is None or not check_password(user.password, password):
+    if user is None or user.password != password: 
         return jsonify({'message': 'Invalid credentials'}), 401
 
     # Generate a JWT token for the user
-    token = jwt.encode({'user_id': user.id, 'exp': datetime.datetime.utcnow() + datetime.timedelta(hours=1)}, app.config['SECRET_KEY'], algorithm='HS256')
+    # token = jwt.encode({'user_id': user.id, 'exp': datetime.datetime.utcnow() + datetime.timedelta(hours=1)}, app.config['SECRET_KEY'], algorithm='HS256')
+    token = create_access_token(identity=user.id)
 
-    return jsonify({'token': token.decode('UTF-8')}), 200
+    # return jsonify({'token': token.decode('UTF-8')}), 200
+    return jsonify({'token': token}), 200
+
+# default jwt_required fn is usualy enough
+# because you are using your own custom one, then this
+# function received current_user as argument.
+@api.route("/private", methods=["GET"])
+@jwt_required() 
+def get_private_data(*args, **kwargs):
+    return jsonify({
+        "user": current_user.serialize(),
+        "data": "some private data."
+    }), 200
--- src/app.py
+++ src/app.py
@@ -7,7 +7,8 @@ from flask_migrate import Migrate
 from flask_swagger import swagger
 from flask_cors import CORS
 from api.utils import APIException, generate_sitemap
-from api.models import db
+from flask_jwt_extended import JWTManager, verify_jwt_in_request, get_jwt_identity
+from api.models import db, User
 from api.routes import api
 from api.admin import setup_admin
 from api.commands import setup_commands
@@ -18,6 +19,41 @@ ENV = "development" if os.getenv("FLASK_DEBUG") == "1" else "production"
 static_file_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), '../public/')
 app = Flask(__name__)
 app.url_map.strict_slashes = False
+
+# @ernestomedinam:
+# this is where you instantiate JWT Manager 
+# and register it to your app. https://flask-jwt-extended.readthedocs.io/en/stable/basic_usage.html
+app.config["JWT_SECRET_KEY"] = os.getenv("FLASK_APP_KEY")
+jwt = JWTManager(app)
+
+# here is the jwt custom loader
+# Define a function to check for the presence of a JWT token in the request headers
+# def token_required(f):
+# @ernestomedinam:
+# use flask-jwt-extended loader to customize jwt_required decorator
+@jwt.user_lookup_loader
+def custom_verify(jwt_headers, jwt_payload): 
+    # @wraps(f)
+    # def decorated(*args, **kwargs):
+    # verifying the token is automatically done by the library
+    # you can customize it with the loader_verification_loader
+    # token = request.headers.get('Authorization')
+    # if not token:
+    #     return jsonify({'message': 'Token is missing'}), 401
+
+    try:
+        # you don't need the jwt object, you can import decode from the library data = jwt.decode(token, app.config['SECRET_KEY'], algorithms=["HS256"])
+        verify_jwt_in_request()
+        user_id = get_jwt_identity()
+        current_user = User.query.filter_by(id=user_id).one_or_none()
+        if current_user is None: 
+            raise Exception("no user")
+    except:
+        return jsonify({'message': 'Token is invalid'}), 401
+
+    return current_user
+
+
 
 # database condiguration
 db_url = os.getenv("DATABASE_URL")
